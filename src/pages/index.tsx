4daea044ff72d363cb0492c5c91be4fa896c2b5c
shunp <shunpei42ba@gmail.com>
2020-07-24 23:15:46 +0900

    add footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -214,6 +214,9 @@ const IndexPage = () => {
           </div>
         </div>
       </div>
+      <div id="footer" className="flex items-center justify-center flex-wrap bg-primary p-2 text-white text-sm bottom-0 w-full mt-4">
+        <span className="">© 2020 Asymmatrix AÖ</span>
+      </div>
     </div>
   )
 }
