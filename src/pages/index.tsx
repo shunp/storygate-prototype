66442d2496b6fc85d69e33d411d5dc744caf592e
shunp <shunpei42ba@gmail.com>
2020-07-25 00:05:46 +0900

    add contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -138,7 +138,7 @@ const IndexPage = () => {
           <div className="tab-content tab-space">
             <div className={openTab === 1 ? 'block' : 'hidden'} id="link1">
               <div className="font-semibold italic bg-primary text-white text-center py-3 shadow-lg">Best Architecture 2018</div>
-              <div>
+              <div className="flex justify-center">
                 <iframe
                   className="relative z-10"
                   title="youtube"
@@ -153,7 +153,7 @@ const IndexPage = () => {
                 AWSが選ぶベストアーキテクチャ2018に僕らのつくった仮想通貨ウォレットシステムが選出されました。日本金融業界からは初選出とのことで嬉しい限りです。
               </div>
               <div className="font-semibold italic bg-primary text-white text-center py-3 shadow-lg">3D WebXR Template</div>
-              <div>
+              <div className="flex justify-center">
                 <TwitterTweetEmbed
                   tweetId="1279046962025652225"
                   options={{ width: size.width }}
@@ -176,7 +176,7 @@ const IndexPage = () => {
               <div className="font-semibold italic bg-primary text-white text-center py-3 shadow-lg">
                 2014/9 (21歳) 社長インタビュアーの小池
               </div>
-              <div>
+              <div className="flex justify-center">
                 <iframe
                   title="facebook"
                   src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fshunpei.koike.9%2Fposts%2F571158393013178"
@@ -191,20 +191,49 @@ const IndexPage = () => {
               <div className="text-gray-500 p-4">
                 大学3年後期、学校の単位を全て取り切り、残りの期間は法人営業で自分を磨くことを決意。当時社長と二人三脚のような零細ベンチャーでの修行が始まった。時給も休みもあまりなかったけれど、大人の世界に背伸びして社長インタビューやスポンサー獲得のために必死に営業した日々はとても疲労に満ちて充実していた。
               </div>
+              <div className="font-semibold italic bg-primary text-white text-center py-3 shadow-lg">2020/1 (26歳) 痛みを伴う脱皮</div>
+              <div className="flex justify-center">
+                <iframe
+                  title="facebook"
+                  src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fshunpei.koike.9%2Fposts%2F2493928454069486"
+                  width={size.width}
+                  height={size.width ? size.width * 0.6 : 0}
+                  scrolling="no"
+                  frameBorder="0"
+                  // allowTransparency="true"
+                  allow="encrypted-media"
+                />
+              </div>
+              <div className="text-gray-500 p-4">
+                2019年の振り返り。個人事業主として独立、海外への挑戦、専門性の確立。とても濃い1年だった。悩んだ時間、価値観について向き合った時間、努力が成果に合わられない時間がとてつもなく大きい一方、1年前からは考えられない距離を自走することができたと思う。{' '}
+              </div>
               <div className="font-semibold italic bg-primary text-white text-center py-3 shadow-lg">2020/5 (26歳) 人工的余白</div>
-              <InstagramEmbed
-                url="https://www.instagram.com/p/CBPLA0eJs5Z/"
-                maxWidth={size.width}
-                hideCaption
-                containerTagName="div"
-                protocol=""
-                onLoading={() => {}}
-                onSuccess={() => {}}
-                onAfterRender={() => {}}
-                onFailure={() => {}}
-              />
+              <div className="flex justify-center">
+                <InstagramEmbed
+                  url="https://www.instagram.com/p/CBPLA0eJs5Z/"
+                  maxWidth={size.width}
+                  hideCaption
+                  containerTagName="div"
+                  protocol=""
+                  onLoading={() => {}}
+                  onSuccess={() => {}}
+                  onAfterRender={() => {}}
+                  onFailure={() => {}}
+                />
+              </div>
               <div className="text-gray-500 p-4">
                 日本を離れマレーシアでの生活。東南アジアの成長スピードと、異文化が交わる都市クアラルンプールの雰囲気に圧倒されつつも、気候も人情もあたたかいこの街がすごく好き。フリーランスエンジニアとしてのポジショニングも安定し、金銭的・時間的余裕を初めてつくり出すことができた。StoryGateを含む多くの活動をこの期間に始める。
+              </div>
+              <div className="font-semibold italic bg-primary text-white text-center py-3 shadow-lg">
+                2020/9 (27歳) フツウのCTOに収まるものか
+              </div>
+              <div className="flex justify-center">
+                <span className="text-lg font-bold text-gray-600 mt-4">香港科技大学で技術研究</span>
+              </div>
+              <div className="text-gray-500 p-4">
+                より専門性を深めるため、アジアトップクラスの大学にて修行することを決意。Asia Future
+                Leadersの100人にアジアから選出。3D技術、ディープラーニング、そしてブロックチェーンを使い、仮想空間には何が必要か技術的に探究予定。to
+                be continued...
               </div>
             </div>
           </div>
