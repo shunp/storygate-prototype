57171d2162e76803b6605ef21b60c17ddf14eb7d
shunp <shunpei42ba@gmail.com>
2020-07-24 19:26:34 +0900

    add fontawesome

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,13 @@ const query = graphql`
         }
       }
     }
+    koike: file(relativePath: { eq: "koike.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
   }
 `
 
@@ -21,14 +28,23 @@ const IndexPage = () => {
     applyTheme(DEFAULT_THEME, themes)
   }, [])
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gradient-r-gray900-primary-gray900 p-1 w-full top-0 fixed z-20">
-      <StaticQuery query={query}
-        render={data => <Img fluid={data.guest.childImageSharp.fluid} className="w-8 h-8 rounded-full z-20 bg-white opacity-50" />
-        } />
-      <div className="flex items-center">
-        <span className="font-semibold text-xl tracking-tight text-white font-serif">StoryGate</span>
+    <div>
+      <nav id="header" className="flex items-center justify-between flex-wrap p-1 mt-2 w-full top-0 fixed z-20">
+        <StaticQuery query={query}
+          render={data => <Img fluid={data.koike.childImageSharp.fluid} className="w-8 h-8 rounded-full z-20 bg-white" />
+          } />
+        <div className="flex items-center">
+          <span className="font-semibold text-3xl tracking-tight font-serif">StoryGate</span>
+        </div>
+        <button type="button" className="inline-block text-xs px-2 py-2 leading-none text-white border-white border rounded">...</button>
+      </nav>
+      <div id="caption" className="flex items-center justify-center flex-col flex-wrap p-4 pt-16">
+        <StaticQuery query={query}
+          render={data => <Img fluid={data.koike.childImageSharp.fluid} className="w-24 h-24 rounded-full z-20 bg-white border-4 border-primary" />
+          } />
+
       </div>
-      <button type="button" className="inline-block text-xs px-2 py-2 leading-none text-white border-white border rounded">LOGIN</button>
-    </nav>)
+    </div>
+  )
 }
 export default IndexPage
