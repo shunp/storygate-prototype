9ded2579fc88920401594590be60ef0bec583b93
shunp <shunpei42ba@gmail.com>
2020-07-24 22:31:57 +0900

    add logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import Img from 'gatsby-image'
 import { graphql, StaticQuery } from 'gatsby'
-import { applyTheme } from '../themes/utils'
 import { themes, DEFAULT_THEME } from 'src/themes'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaw, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { TwitterTweetEmbed } from 'react-twitter-embed'
+import { applyTheme } from '../themes/utils'
 
 const query = graphql`
   query {
@@ -16,6 +17,13 @@ const query = graphql`
       }
     }
     koike: file(relativePath: { eq: "koike.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    logo: file(relativePath: { eq: "logo.png" }) {
       childImageSharp {
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
@@ -38,7 +46,7 @@ const useWindowSize = () => {
 
   React.useEffect(() => {
     if (!isClient) {
-      return false;
+      return false
     }
 
     function handleResize() {
@@ -56,24 +64,31 @@ const IndexPage = () => {
   React.useEffect(() => {
     applyTheme(DEFAULT_THEME, themes)
   }, [])
-  const [openTab, setOpenTab] = React.useState(1);
+  const [openTab, setOpenTab] = React.useState(1)
   const size = useWindowSize()
   return (
     <div>
-      <nav id="header" className="flex items-center justify-between flex-wrap p-1 mt-2 w-full top-0 fixed z-20">
-        <StaticQuery query={query}
-          render={data => <Img fluid={data.koike.childImageSharp.fluid} className="w-8 h-8 rounded-full z-20 bg-white" />
-          } />
+      <nav id="header" className="flex items-center justify-between flex-wrap p-2 w-full top-0 fixed z-20 bg-white">
+        <StaticQuery
+          query={query}
+          render={data => <Img fluid={data.koike.childImageSharp.fluid} className="w-8 h-8 rounded-full z-20 bg-white" />}
+        />
         <div className="flex items-center">
-          <span className="font-semibold text-3xl tracking-tight font-serif">StoryGate</span>
+          {/* <span className="font-semibold text-3xl tracking-tight font-serif">StoryGate</span> */}
+          <StaticQuery query={query} render={data => <Img fluid={data.logo.childImageSharp.fluid} className="w-40 z-20 bg-white" />} />
         </div>
-        <button type="button" className="inline-block text-sm px-2 py-2 leading-none text-black border-white border rounded">...</button>
+        <button type="button" className="inline-block text-sm px-2 py-2 leading-none text-black border-white border rounded">
+          ...
+        </button>
       </nav>
       <div id="caption" className="flex items-center justify-center flex-col flex-wrap p-4 pt-16">
         <div id="profile-image" className="flex items-center justify-between flex-wrap w-1/2">
-          <StaticQuery query={query}
-            render={data => <Img fluid={data.koike.childImageSharp.fluid} className="w-24 h-24 rounded-full z-20 bg-white border-4 border-primary" />
-            } />
+          <StaticQuery
+            query={query}
+            render={data => (
+              <Img fluid={data.koike.childImageSharp.fluid} className="w-24 h-24 rounded-full z-20 bg-white border-4 border-primary" />
+            )}
+          />
           <FontAwesomeIcon icon={faPaw} size="2x" className="text-primary" />
         </div>
         <div id="profile-name" className="mt-2">
@@ -84,19 +99,19 @@ const IndexPage = () => {
           <span className="mx-1 text-gray-500">Hong Kong</span>
         </div>
         <div id="profile-introduction" className="mt-2">
-          <span className="text-gray-500">BlockchainやWebGLなど / AWS Best Architecture 2018 / 書籍「Solidityプログラミング」発売中 / 秋から香港で仮想世界構築の研究</span>
+          <span className="text-gray-500">
+            BlockchainやWebGLなど / AWS Best Architecture 2018 / 書籍「Solidityプログラミング」発売中 / 秋から香港で仮想世界構築の研究
+          </span>
         </div>
       </div>
       <div id="main-tab" className="flex flex-wrap">
         <div className="w-full">
           <ul className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row" role="tablist">
             <li className="mx-1 flex-auto text-center">
-              <a className={
-                "text-xs font-bold uppercase px-2 py-2 shadow-lg rounded block leading-normal " +
-                (openTab === 1
-                  ? "text-white bg-primary"
-                  : "text-primary bg-white")
-              }
+              <a
+                className={`text-xs font-bold uppercase px-2 py-2 shadow-lg rounded block leading-normal ${
+                  openTab === 1 ? 'text-white bg-primary' : 'text-primary bg-white'
+                }`}
                 onClick={e => {
                   e.preventDefault()
                   setOpenTab(1)
@@ -109,12 +124,10 @@ const IndexPage = () => {
               </a>
             </li>
             <li className="mx-1 flex-auto text-center">
-              <a className={
-                "text-xs font-bold uppercase px-5 py-2 shadow-lg rounded block leading-normal " +
-                (openTab === 2
-                  ? "text-white bg-primary"
-                  : "text-primary bg-white")
-              }
+              <a
+                className={`text-xs font-bold uppercase px-5 py-2 shadow-lg rounded block leading-normal ${
+                  openTab === 2 ? 'text-white bg-primary' : 'text-primary bg-white'
+                }`}
                 onClick={e => {
                   e.preventDefault()
                   setOpenTab(2)
@@ -132,10 +145,8 @@ const IndexPage = () => {
       <div id="main-content" className="relative flex flex-col w-full">
         <div className="mt-4 flex-auto">
           <div className="tab-content tab-space">
-            <div className={openTab === 1 ? "block" : "hidden"} id="link1">
-              <div className="font-semibold italic bg-primary text-white text-center py-3 shadow-lg">
-                Best Architecture 2018
-              </div>
+            <div className={openTab === 1 ? 'block' : 'hidden'} id="link1">
+              <div className="font-semibold italic bg-primary text-white text-center py-3 shadow-lg">Best Architecture 2018</div>
               <div>
                 <iframe
                   className="relative z-10"
@@ -143,12 +154,34 @@ const IndexPage = () => {
                   id="ytplayer"
                   width={size.width}
                   height={size.width ? size.width * 0.6 : 0}
-                  src={`https://www.youtube.com/embed/WlkWTye4mfI`}
+                  src="https://www.youtube.com/embed/WlkWTye4mfI"
                   frameBorder="0"
                 />
               </div>
+              <div className="text-gray-500 p-4">
+                AWSが選ぶベストアーキテクチャ2018に僕らのつくった仮想通貨ウォレットシステムが選出されました。日本金融業界からは初選出とのことで嬉しい限りです。
+              </div>
+              <div className="font-semibold italic bg-primary text-white text-center py-3 shadow-lg">3D WebXR Template</div>
+              <div>
+                <TwitterTweetEmbed
+                  tweetId="1279046962025652225"
+                  options={{ width: size.width }}
+                  onLoad={tweetWidgetEl => {
+                    if (tweetWidgetEl.children[0]) {
+                      tweetWidgetEl.children[0].style.width = `${size.width}px`
+                    }
+                    if (tweetWidgetEl.shadowRoot) {
+                      const tweetEl = tweetWidgetEl.shadowRoot.querySelector('.SandboxRoot')
+                      tweetEl.style.position = 'static'
+                    }
+                  }}
+                />
+              </div>
+              <div className="text-gray-500 p-4">
+                AWSが選ぶベストアーキテクチャ2018に僕らのつくった仮想通貨ウォレットシステムが選出されました。日本金融業界からは初選出とのことで嬉しい限りです。
+              </div>
             </div>
-            <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+            <div className={openTab === 2 ? 'block' : 'hidden'} id="link2">
               yyy
             </div>
           </div>
--- src/themes/base.ts
+++ src/themes/base.ts
@@ -1,5 +1,5 @@
 export default {
-  primary: '#0bccc7',
+  primary: '#5DC1CF',
   secondary: '#254E70',
   negative: '#f56565',
   positive: '#38b2ac',
