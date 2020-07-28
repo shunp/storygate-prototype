4690a4c558a6c60f859c43efd8857033f2c45597
shunp <shunpei42ba@gmail.com>
2020-07-28 23:48:26 +0900

    add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Footer = () => {
+  return (
+    <div id="footer" className="flex items-center justify-center flex-wrap bg-primary p-2 text-white text-sm bottom-0 w-full mt-4">
+      <span className="">© 2020 Asymmatrix OÜ</span>
+    </div>
+  )
+}
+
+export default Footer
