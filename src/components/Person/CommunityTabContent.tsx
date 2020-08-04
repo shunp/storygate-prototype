48a26ac91d0acea78015260af407e8ecd5248328
shunp <shunpei42ba@gmail.com>
2020-08-05 02:18:07 +0900

    fix community components

--- src/components/Person/CommunityTabContent.tsx
+++ src/components/Person/CommunityTabContent.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react'
-import { toggleEditStory, toggleEditPortfolio } from 'src/state/app'
 import { AnyAction } from 'redux'
-import EditButton from '../EditButton'
-import ClearButton from '../ClearButton'
-import DoneButton from '../DoneButton'
-import { NewPortfolio, PortfolioList } from '../Content/Portfolio'
-import { Story, AddStory } from '../Content/Story'
 import { CommunityList } from '../Content/Community'
 
 const CommunityTabContent = ({ index, openTab, size, editing, dispatch }) => {
+  // TODO: DB
+  // show: can see the content if the login user is one of the members of the community
   const communityData = [
     {
       title: '西野亮廣エンタメ研究所',
       number: 70000,
       link: 'nishinosalon',
       pic: '',
+      show: true,
       text:
         '日本を離れマレーシアでの生活。東南アジアの成長スピードと、異文化が交わる都市クアラルンプールの雰囲気に圧倒されつつも、気候も人情もあたたかいこの街がすごく好き。フリーランスエンジニアとしてのポジショニングも安定し、金銭的・時間的余裕を初めてつくり出すことができた。StoryGateを含む多くの活動をこの期間に始める。'
     },
@@ -23,6 +20,7 @@ const CommunityTabContent = ({ index, openTab, size, editing, dispatch }) => {
       number: 15,
       link: 'stroygate',
       pic: '',
+      show: true,
       text:
         '日本を離れマレーシアでの生活。東南アジアの成長スピードと、異文化が交わる都市クアラルンプールの雰囲気に圧倒されつつも、気候も人情もあたたかいこの街がすごく好き。フリーランスエンジニアとしてのポジショニングも安定し、金銭的・時間的余裕を初めてつくり出すことができた。StoryGateを含む多くの活動をこの期間に始める。'
     },
@@ -31,12 +29,20 @@ const CommunityTabContent = ({ index, openTab, size, editing, dispatch }) => {
       number: 40,
       link: 'hongkong',
       pic: '',
+      show: false,
       text:
         '日本を離れマレーシアでの生活。東南アジアの成長スピードと、異文化が交わる都市クアラルンプールの雰囲気に圧倒されつつも、気候も人情もあたたかいこの街がすごく好き。フリーランスエンジニアとしてのポジショニングも安定し、金銭的・時間的余裕を初めてつくり出すことができた。StoryGateを含む多くの活動をこの期間に始める。'
     }
   ]
+  if (editing) {
+    return (
+      <div className={openTab === index ? 'block' : 'hidden'} id={`link${index}`}>
+        <CommunityList data={communityData} size={size} />
+      </div>
+    )
+  }
   return (
-    <div className={openTab === index ? 'block' : 'hidden'} id="link3">
+    <div className={openTab === index ? 'block' : 'hidden'} id={`link${index}`}>
       <CommunityList data={communityData} size={size} />
     </div>
   )
