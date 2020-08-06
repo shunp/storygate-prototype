d280520f5471c6afe4201a790bada65e3eff04d9
shunp <shunpei42ba@gmail.com>
2020-08-06 22:05:50 +0900

    add navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react'
 import { css } from '@emotion/core'
 import Img from 'gatsby-image'
-import { useStaticQuery, graphql, Link } from 'gatsby'
+import { useStaticQuery, graphql, Link, navigate } from 'gatsby'
+import { State } from 'src/state'
+
 import { Background, LoginTitleLine, Heading, Logo } from 'src/components/Auth/top'
+import { AuthService } from 'src/services/AuthService'
+import { LoginUser } from 'src/services/interfaces/Auth'
+import { connect } from 'react-redux'
+import { publishLogin } from 'src/state/app'
+
+interface HeaderStates {
+  loginUser: LoginUser
+}
+interface HeaderDispatch {
+  login: (logingUser: LoginUser) => void
+}
 
 const MailAddress = ({ emailAddress, setEmailAddress }) => {
   return (
@@ -49,26 +62,27 @@ const MailLoginButton = ({ emailAddress, password }) => {
   )
 }
 
-const FacebookLoginButton = () => {
+const onClickLogin = async (login: (loginUser: LoginUser) => void) => {
+  const loginUser = await AuthService.login()
+  if (!loginUser) {
+    return
+  }
+  login(loginUser)
+  console.log('loginUser', loginUser)
+  // TODO:
+  // navigate(`/persons/${loginUser.uid}`)
+  navigate('/persons/owner')
+}
+
+const FacebookLoginButton = ({ login }) => {
   return (
-    <button type="button" className="rounded-full text-white bg-blue-800 py-2 px-10 m-2 w-full">
+    <button type="button" className="rounded-full text-white bg-blue-800 py-2 px-10 m-2 w-full" onClick={() => onClickLogin(login)}>
       Facebookでログイン
     </button>
   )
 }
 
-const IndexPage = () => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     background: file(relativePath: { eq: "nishinosalon_background.jpg" }) {
-  //       childImageSharp {
-  //         fluid(maxWidth: 1000) {
-  //           ...GatsbyImageSharpFluid
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+const IndexPage = ({ login }) => {
   // TODO: from GraphQL Schema
   const url = `https://firebasestorage.googleapis.com/v0/b/story-gate.appspot.com/o/443502378%2Fbackground.jpg?alt=media&token=d51ed645-6e89-4475-aef5-695efba575ba`
   const [emailAddress, setEmailAddress] = React.useState('')
@@ -93,7 +107,7 @@ const IndexPage = () => {
           </div>
           <div className="flex justify-center text-white">または</div>
           <div className="flex justify-center">
-            <FacebookLoginButton />
+            <FacebookLoginButton login={login} />
           </div>
         </div>
         <div className="border-white border-solid border-2 mx-4 mt-4 opacity-50 lg:max-w-3xl lg:mx-auto" />
@@ -108,4 +122,11 @@ const IndexPage = () => {
   )
 }
 
-export default IndexPage
+export default connect<HeaderStates, HeaderDispatch, {}, State>(
+  state => ({
+    loginUser: state.app.loginUser
+  }),
+  dispatch => ({
+    login: (loginUser: LoginUser) => dispatch(publishLogin(loginUser))
+  })
+)(IndexPage)
