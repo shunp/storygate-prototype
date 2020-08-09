faa251836eed1ead0ed8d64be56e103fb9cfb87d
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-08-09 20:52:43 +0900

    enable to add twitter

--- src/utils/ContentsExtractor.tsx
+++ src/utils/ContentsExtractor.tsx
@@ -7,13 +7,21 @@ const judgeType = (url: URL): string => {
   if (hostname.includes('youtube')) {
     return 'YouTubePost'
   }
+  if (hostname.includes('twitter.com')) {
+    return 'TwitterPost'
+  }
   return 'GeenralURL'
 }
 
 const YouTubeUrlRegex = /(?:\/|[?&]v=)([-\w]{11})(?:\/|\?|&|$)/
+const TwitterUrlRegex = /status\/([0-9]*)(?:\?|$)/
 const extractKey = (type: string, url: string): string => {
   if (type === 'YouTubePost') {
     const result = YouTubeUrlRegex.exec(url)
+    return (result?.length && result[1]) || ''
+  }
+  if (type === 'TwitterPost') {
+    const result = TwitterUrlRegex.exec(url)
     return (result?.length && result[1]) || ''
   }
   return url
