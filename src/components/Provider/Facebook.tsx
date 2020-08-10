21c82f7c9d57acac2add52726f4065405556a820
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-08-10 15:09:33 +0900

    enable to facebook and instagram

--- src/components/Provider/Facebook.tsx
+++ src/components/Provider/Facebook.tsx
@@ -1,6 +1,24 @@
 import * as React from 'react'
 import { IframeProps } from 'src/services/interfaces/Provider'
+import { WithIFrame } from 'src/services/interfaces/Portfolio'
 import { ContentExplanation } from '../ContentExplanation'
+import { PortfolioContentProps, asPortfolioContentIFrame } from '../Person/Portfolio/PortfolioContentBase'
+
+const Iframe: React.FC<PortfolioContentProps<WithIFrame>> = ({ iframeKey, size }) => {
+  return (
+    <iframe
+      title="facebook"
+      src={`https://www.facebook.com/plugins/post.php?href=${iframeKey}`}
+      width={size}
+      height={size ? size * 0.6 : 0}
+      scrolling="no"
+      frameBorder="0"
+      // allowTransparency="true"
+      allow="encrypted-media"
+    />
+  )
+}
+export const FacebookIFrame = asPortfolioContentIFrame(Iframe)
 
 export const FacebookPost: React.FC<IframeProps> = ({ TitleLine, iframeKey, text, size }) => {
   return (
