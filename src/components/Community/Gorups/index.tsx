5687fc2bfaf07c3e4dc920f5e54ce0ff0016dd7a
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-08-16 02:03:48 +0900

    display num of members og group

--- src/components/Community/Gorups/index.tsx
+++ src/components/Community/Gorups/index.tsx
@@ -1,30 +1,33 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import { GroupReference } from 'src/services/interfaces/GroupCaption'
+import { display } from 'src/utils/numeral'
 import { CreateGroupIcon } from './CreateNewGroup'
 
 interface GroupProps {
   name: string
   pageId: string
+  num: number
   img?: string
 }
-const GroupView: React.FC<GroupProps> = ({ name, pageId, img }) => {
+const GroupView: React.FC<GroupProps> = ({ name, pageId, num, img }) => {
   return (
     <div className="p-3 w-1/3">
       <Link to={`/groups/${pageId}`} className="flex flex-col items-center">
         <img src={img} width={140} alt="" className="w-24 h-24 rounded-full" />
         <div>{name}</div>
+        <div>{display(num)}人</div>
       </Link>
     </div>
   )
 }
 interface GroupsProps {
   groups: GroupReference[]
-  createNewGroup: (name: string, backgroundImg?: Blob) => Promise<void>
+  createNewGroup: (name: string, introduction: string, backgroundImg?: Blob) => Promise<void>
 }
 export const Groups: React.FC<GroupsProps> = ({ groups, createNewGroup }) => {
   const GroupComponents = groups.map(group => {
-    return <GroupView name={group.name} pageId={group.pageId} img={group.backgroundImg} />
+    return <GroupView name={group.name} pageId={group.pageId} num={group.numOfMembers} img={group.backgroundImg} />
   })
   return (
     <>
