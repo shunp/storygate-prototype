b28f4a3e0595338e2812a6b4be82602b55c25372
shunp <shunpei42ba@gmail.com>
2020-08-18 10:49:59 +0900

    add about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,27 +13,41 @@ const Recruit = () => {
           height: 100%;
         `}
       >
-        <div className="flex justify-center items-center mt-40 mb-10">
-          <Montserrat className="text-white text-3xl font-bold">学生インターン募集</Montserrat>
-        </div>
-        <div className="flex justify-center items-center mx-5">
-          <Montserrat className="text-gray-500">StoryGateは現在クローズドα版です。</Montserrat>
+        <div className="flex justify-center items-center mt-10 mb-10">
+          <Montserrat className="text-white text-3xl font-bold">StoryGateとは？</Montserrat>
         </div>
         <div className="flex justify-center items-center mx-5">
           <Montserrat className="text-gray-500 text-center">
-            より良いサービスを提供するため、またオンラインコミュニティ市場拡大のために、私たち平均年齢26歳の開発チームと一緒に働いてみませんか？
+            StoryGateは世界初のオンラインコミュニティ専用ソーシャルツールです。パブリックのSNSとプライベートSNSは公開できる情報に大きな隔たりがあるはずですが、現在の主要なプラットフォームでは基本的に全てパブリック専用での利用が想定されています。私たちは個人のより深い情報を同じコミュニティの人と共有しあえるコミュニティ運営の機会を提供していきます。
           </Montserrat>
         </div>
-        <div className="flex justify-center items-center mx-5">
-          <Montserrat className="text-gray-500 text-center">
-            企画、開発、デザイン、広報、チームマネジメントなど、様々な分野でご参加いただけます。
-          </Montserrat>
+        <div className="flex justify-center items-center mt-10 mb-10">
+          <Montserrat className="text-white text-3xl font-bold">運営会社</Montserrat>
         </div>
-        <div className="flex justify-center flex-col mt-10">
+        <div>
+          <div className="flex justify-start items-center">
+            <Montserrat className="text-white rounded-full bg-gradient-t-blue-pink-purple py-1 px-6 ml-10 mr-4 text-sm">商　号</Montserrat>
+            <Montserrat className="text-white font-bold">Asymmatrix OÜ</Montserrat>
+          </div>
+          <div className="flex justify-start items-center mt-3">
+            <Montserrat className="text-white rounded-full bg-gradient-t-blue-pink-purple py-1 px-6 ml-10 mr-4 text-sm">資本金</Montserrat>
+            <Montserrat className="text-white font-bold">5,000,000円</Montserrat>
+          </div>
+          <div className="flex justify-start items-center mt-3">
+            <Montserrat className="text-white rounded-full bg-gradient-t-blue-pink-purple py-1 px-6 ml-10 mr-4 text-sm">代表者</Montserrat>
+            <Montserrat className="text-white font-bold">小池駿平</Montserrat>
+          </div>
+          <div className="flex justify-start items-center mt-3">
+            <Montserrat className="text-white rounded-full bg-gradient-t-blue-pink-purple py-1 px-6 ml-10 mr-4 text-sm">設　立</Montserrat>
+            <Montserrat className="text-white font-bold">2020年7月22日</Montserrat>
+          </div>
+          <div className="flex justify-start items-center mt-3">
+            <Montserrat className="text-white rounded-full bg-gradient-t-blue-pink-purple py-1 px-6 ml-10 mr-4 text-sm">連絡先</Montserrat>
+            <Montserrat className="text-white font-bold">contact@storygate.info</Montserrat>
+          </div>
+          <div className="border-white border-solid border mx-4 mt-10 opacity-50 lg:max-w-3xl lg:mx-auto" />
           <div className="flex justify-center">
-            <button type="button" className="rounded-full bg-gradient-t-blue-pink-purple py-2 px-10 m-2 w-full text-white">
-              参加したい
-            </button>
+            <Montserrat className="text-white text-sm font-bold opacity-75 mt-4">© 2020 Asymmatrix OÜ</Montserrat>
           </div>
         </div>
       </div>
