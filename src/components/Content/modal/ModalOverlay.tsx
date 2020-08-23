2d8489ef834a375ceaac02c548f5fc5c428028a1
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-08-23 20:12:16 +0900

    enable to call function on focut out modal

--- src/components/Content/modal/ModalOverlay.tsx
+++ src/components/Content/modal/ModalOverlay.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react'
 import { togglePostModal } from './utils'
 
-const ModalOverlay = ({ id }) => {
+interface ModalOverlayProps {
+  id: string
+  onFocusOut?: () => void
+}
+const ModalOverlay: React.FC<ModalOverlayProps> = ({ id, onFocusOut }) => {
   return (
-    <div className="absolute w-full h-full bg-gray-900 opacity-90 z-10" onClick={() => togglePostModal(id)} />
+    <div
+      className="absolute w-full h-full bg-gray-900 opacity-90 z-10"
+      onClick={() => {
+        if (onFocusOut) {
+          onFocusOut()
+        }
+        togglePostModal(id)
+      }}
+    />
     // <button type="button" onClick={() => toggleLoginModal()}>
     //   <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50" />
     // </button>
