4e5948cafca7aedc1f246d49d8540b32ed12ac32
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-08-23 14:17:44 +0900

    decorate links in text

--- src/components/TextWithLink.tsx
+++ src/components/TextWithLink.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react'
 import Linkify from 'react-linkify'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+
+const componentDecorator = (decoratedHref: string, decoratedText: string, key: number): React.ReactNode => (
+  <a href={decoratedHref} key={key} target="_blank" rel="noopener noreferrer">
+    <FontAwesomeIcon icon={faLink} className="text-gray-500 mr-2" />
+    {decoratedText}
+  </a>
+)
 
 interface TextWithLinkProps {
   text: string
 }
 export const TextWithLink: React.FC<TextWithLinkProps> = ({ text }) => {
-  return <Linkify>{text}</Linkify>
+  return (
+    <Linkify componentDecorator={componentDecorator}>
+      <div className="whitespace-pre-wrap">{text}</div>
+    </Linkify>
+  )
 }
