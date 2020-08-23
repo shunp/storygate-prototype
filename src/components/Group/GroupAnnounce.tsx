5fb58f96a3b3389296cd8de8f07e501f2265181a
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-08-23 14:38:56 +0900

    refactor annouce

--- src/components/Group/GroupAnnounce.tsx
+++ src/components/Group/GroupAnnounce.tsx
@@ -15,8 +15,37 @@ const ModifiableAnnoucement: React.FC<ModifiableAnnoucementProps> = ({ message, 
     className="border-2 border-gray-300 focus:outline-none bg-white w-full h-64 px-2 rounded-lg whitespace-pre-wrap"
     value={message}
     onChange={e => onChange(e.target.value)}
+    placeholder="announcement..."
   />
 )
+
+interface AnnoucementComponentProps {
+  announcement?: Announcement
+  joined: boolean
+  onEditButtonClick: () => void
+}
+const AnnoucementComponent: React.FC<AnnoucementComponentProps> = ({ announcement, joined, onEditButtonClick }) => {
+  if (!joined && !announcement) {
+    return <></>
+  }
+  if (!announcement) {
+    return <FontAwesomeIcon size="2x" className="text-gray-500 m-2" icon={faEdit} onClick={onEditButtonClick} />
+  }
+  return (
+    <div id="group-announcement" className="max-w-sm rounded overflow-hidden shadow-lg w-full">
+      <div className="p-4">
+        <div className="flex justify-between text-gray-500">
+          <div>{announcement.createdAt.format()}</div>
+          {joined ? <FontAwesomeIcon icon={faEdit} onClick={onEditButtonClick} /> : ''}
+        </div>
+        <div className="p-2">
+          <TextWithLink text={announcement.message} />
+        </div>
+        <div className="text-gray-500 text-right">{announcement.authorName}</div>
+      </div>
+    </div>
+  )
+}
 interface GroupAnnounceProps {
   announcement?: Announcement
   joined: boolean
@@ -28,31 +57,17 @@ export const GroupAnnounce: React.FC<GroupAnnounceProps> = ({ announcement, join
   React.useEffect(() => {
     setMessage(announcement?.message || '')
   }, [announcement])
-  if (!announcement) {
-    return <></>
-  }
   return (
     <>
       <BasicTitleLine title="Announcement" />
-      <div id="group-announcement" className="max-w-sm rounded overflow-hidden shadow-lg w-full">
-        <div className="p-4">
-          <div className="flex justify-between text-gray-500">
-            <div>{announcement.createdAt.format()}</div>
-            {joined ? <FontAwesomeIcon icon={faEdit} onClick={() => setEditing(!editing)} /> : ''}
-          </div>
-          <div className="p-2">
-            <TextWithLink text={announcement.message} />
-          </div>
-          <div className="text-gray-500 text-right">{announcement.authorName}</div>
-        </div>
-        <DefaultPostModal
-          id="group-annouce"
-          editing={editing}
-          onClear={() => setMessage(announcement.message)}
-          onDone={() => updateAnnouncement(message)}
-          Post={<ModifiableAnnoucement message={message} onChange={updateMessage => setMessage(updateMessage)} />}
-        />
-      </div>
+      <AnnoucementComponent announcement={announcement} joined={joined} onEditButtonClick={() => setEditing(true)} />
+      <DefaultPostModal
+        id="group-annouce"
+        editing={editing}
+        onClear={() => setMessage(announcement?.message || '')}
+        onDone={() => updateAnnouncement(message)}
+        Post={<ModifiableAnnoucement message={message} onChange={updateMessage => setMessage(updateMessage)} />}
+      />
     </>
   )
 }
