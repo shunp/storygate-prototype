745a0e2c58b333d0a1b9903be9d0a1019a9534bf
touka-nt@gmail.com <sss.d1153@gmail.com>
2020-08-23 14:15:51 +0900

    add DefaultModal

--- src/components/Content/modal/PostModal.tsx
+++ src/components/Content/modal/PostModal.tsx
@@ -1,13 +1,63 @@
 import * as React from 'react'
+import { CompleteButtonSet } from 'src/components/EditButton'
+import ClearButton from 'src/components/ClearButton'
+import DoneButton from 'src/components/DeleteButton'
 import PostModalWrapper from './PostModalWrapper'
 import ModalOverlay from './ModalOverlay'
 import ModalContainer from './ModalContainer'
+import { togglePostModal } from './utils'
 
-const PostModal = ({ Post, id }) => {
+interface PostModalProps {
+  Post: JSX.Element
+  id: string
+}
+const PostModal: React.FC<PostModalProps> = ({ Post, id }) => {
   return (
     <PostModalWrapper id={id}>
       <ModalOverlay id={id} />
       <ModalContainer>{Post}</ModalContainer>
+    </PostModalWrapper>
+  )
+}
+
+type DefaultPostModalProps = PostModalProps & {
+  editing: boolean
+  onClear: () => void
+  onDone: () => Promise<void>
+}
+export const DefaultPostModal: React.FC<DefaultPostModalProps> = ({ Post, id, editing, onClear, onDone }) => {
+  React.useEffect(() => {
+    if (editing) {
+      togglePostModal(id)
+    }
+  }, [editing])
+  return (
+    <PostModalWrapper id={id}>
+      <ModalOverlay id={id} />
+      <ModalContainer>
+        <>
+          <CompleteButtonSet
+            ClearButton={
+              <ClearButton
+                onClick={() => {
+                  onClear()
+                  togglePostModal(id)
+                }}
+              />
+            }
+            DoneButton={
+              <DoneButton
+                onClick={async () => {
+                  await onDone()
+                  togglePostModal(id)
+                }}
+              />
+            }
+            className="mt-1"
+          />
+          {Post}
+        </>
+      </ModalContainer>
     </PostModalWrapper>
   )
 }
